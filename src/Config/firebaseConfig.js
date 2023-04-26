3b5488ea7f3a1221bb5b98828e9349a241cedaaa
rishi29dec <rishi29dec@gmail.com>
2023-04-26 18:23:06 +0530

    first commit

---
+++ src/Config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDDTu5uDbyNAwVFPnCAEG50BdPcd00hcfw",
+  authDomain: "crypto-verse-b4bda.firebaseapp.com",
+  projectId: "crypto-verse-b4bda",
+  storageBucket: "crypto-verse-b4bda.appspot.com",
+  messagingSenderId: "139835874054",
+  appId: "1:139835874054:web:06286242341b4e0d591c87"
+};
+
+  export default firebaseConfig;  
